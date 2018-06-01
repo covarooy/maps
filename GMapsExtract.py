2c6406a28441c772de53a8ed36032e42fdca3cca
covarooy <cobusvanrooy@gmail.com>
2018-06-01 15:00:46 +0200

    Update GMapsExtract.py

--- GMapsExtract.py
+++ GMapsExtract.py
@@ -1,5 +1,5 @@
 '''
-Google Maps Path: https://maps.googleapis.com/maps/api/elevation/json?path=-33.941033,18.457269|-34.075809,18.296103&samples=300&key=AIzaSyDkVBeSmR4wP1YpC7weGvIbvJnLsXyTE_Y
+Google Maps Path: https://maps.googleapis.com/maps/api/elevation/json?path=-33.941033,18.457269|-34.075809,18.296103&samples=300&key=
 
 '''
 
